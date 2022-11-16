f71c81c7d83f063f5161b51ce17c8dbd86fd0d48
Arda Çabuk <114997916+ArdaCabuk@users.noreply.github.com>
2022-11-16 20:40:06 +0300

    Update Soru12.py

--- Soru12.py
+++ Soru12.py
@@ -1,7 +1,6 @@
 for i in range(1,2005):
-    x = 0
+    x=0
     for y in range(len(str(i))):
-        x += int(str(i)[y])
-    if x == 2005- i :
+        x+=int(str(i)[y])
+    if x==2005-i:
         print(i)
-
